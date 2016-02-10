451a4fb9fe1b0323942246484da192e9be07c0ac
Kevin Hankens <email@kevinhankens.com>
2016-02-10 14:29:23 -0500

    Updating php and bower.

--- check-php-extensions-versions.php
+++ check-php-extensions-versions.php
@@ -8,11 +8,11 @@
 function get_php_ext_versions() {
   $extensions = array(
     'gd' => '2.1.0',
-    'pdo_mysql' => '5.0.11-dev - 20120503 - $Id: 3c688b6bbc30d36af3ac34fdd4b7b5b787fe5555',
+    'pdo_mysql' => '5.0.11-dev - 20120503 - $Id: 76b08b24596e12d4553bd41fc93cccd5bac2fe7a',
     'mcrypt' => '2.5.8',
     'gmp' => '6.0.0',
-    'zip' => 'c203148334b6f80d27bc5d23fad5ec3ca7dcf444',
-    'mysqli' => '5.0.11-dev - 20120503 - $Id: 3c688b6bbc30d36af3ac34fdd4b7b5b787fe5555',
+    'zip' => 'd040d206d1a9ea6ebaaa007ad186da532d8ba024',
+    'mysqli' => '5.0.11-dev - 20120503 - $Id: 76b08b24596e12d4553bd41fc93cccd5bac2fe7a',
     'bz2' => '1.0.6',
   );
 
@@ -22,7 +22,7 @@ function get_php_ext_versions() {
     $ext->info();
     $ext_info = ob_get_contents();
     ob_end_clean();
-    
+
     $version_matches = strpos($ext_info, $version);
     if ($version_matches === FALSE) {
       printf("Version check failed for %s. Expected %s, got %s.\n", $extension, $version, $ext_info);
