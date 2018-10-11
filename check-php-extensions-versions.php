d129853cec02e0de0b2635fbdb778b78a87719b9
SMAWI <fayssal.smawi@gmail.com>
2018-10-11 15:32:34 +0100

    MS-3864|update version for FIPS container (#228)
    * MS-3864|update version for FIPS container
    
    * updated versions

--- check-php-extensions-versions.php
+++ check-php-extensions-versions.php
@@ -7,15 +7,15 @@
  */
 function get_php_ext_versions() {
   $extensions = array(
-    'gd' => '2.1.1-dev',
+    'gd' => '2.1.1',
     'pdo_mysql' => '5.0.11-dev - 20120503 - $Id: 76b08b24596e12d4553bd41fc93cccd5bac2fe7a',
     'mcrypt' => '2.5.8',
-    'gmp' => '6.0.0',
+    'gmp' => '6.1.0',
     'zip' => '1.12.5',
     'mysqli' => '5.0.11-dev - 20120503 - $Id: 76b08b24596e12d4553bd41fc93cccd5bac2fe7a',
     'bz2' => '1.0.6',
     'imagick' => '3.4.3',
-    'curl' => '7.38.0',
+    'curl' => '7.47.0',
   );
 
   foreach ($extensions as $extension => $version) {
--- check-php-extensions-versions7.php
+++ check-php-extensions-versions7.php
@@ -7,15 +7,15 @@
  */
 function get_php_ext_versions() {
   $extensions = array(
-    'gd' => '2.1.1-dev',
+    'gd' => '2.1.1',
     'pdo_mysql' => '5.0.12-dev - 20150407 - $Id: 38fea24f2847fa7519001be390c98ae0acafe387',
     'mcrypt' => '2.5.8',
-    'gmp' => '6.0.0',
+    'gmp' => '6.1.0',
     'zip' => '1.13.5',
     'mysqli' => '5.0.12-dev - 20150407 - $Id: 38fea24f2847fa7519001be390c98ae0acafe387',
     'bz2' => '1.0.6',
     'imagick' => '3.4.3',
-    'curl' => '7.38.0',
+    'curl' => '7.47.0',
   );
 
   foreach ($extensions as $extension => $version) {
